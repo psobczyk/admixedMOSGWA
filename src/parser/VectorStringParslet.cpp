5a279e25dc693f1f15646cc6f3960ee2a5967956
Bernhard Bodenstorfer <mosgwa@cogiti.com>
2013-12-02 15:18:31 +0100

    Erich added output to cerr.

--- src/parser/VectorStringParslet.cpp
+++ src/parser/VectorStringParslet.cpp
@@ -26,21 +26,21 @@ namespace parser {
 		if ( '{' != *text ) return false;
 		text++;
 			skipWhitespace(text); //erase withespace
-                 cerr<<"0:"<<text<<endl;
+               //  cerr<<"0:"<<text<<endl;
 		 bool debug1='}'!= *text;
-cerr<< debug1 <<endl;
+//cerr<< debug1 <<endl;
 		while ('}'!= *text )
 		{//skipWhitespace(text); 
 		if ( '"' != *text ) return false;
-		cerr<<"2:";
+//		cerr<<"2:";
 		text++;
-		cerr<<"3:"<<text<<endl;
+//		cerr<<"3:"<<text<<endl;
 		const char* endQuote = strchr( text, '"' );
 		if ( endQuote == text ) return false;
 		 strv.push_back(string( text , endQuote - text /* - 1*/ ));
-		 cerr<<"5:"<<string( text , endQuote - text /*- 1*/ )<<endl;
+//		 cerr<<"5:"<<string( text , endQuote - text /*- 1*/ )<<endl;
 		text = endQuote + 1;
-cerr<<"3:"<<text<<endl;
+//cerr<<"3:"<<text<<endl;
                 skipWhitespace(text);
                 if ( ',' == *text)
 		         text++; //skip , 
