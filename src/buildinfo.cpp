6f8d8bc14de71f788d7589cb0b0d67f970efac2e
Bernhard Bodenstorfer <mosgwa@cogiti.com>
2014-08-13 16:23:59 +0200

    New version with genetic algorithms.

--- src/buildinfo.cpp
+++ src/buildinfo.cpp
@@ -15,5 +15,5 @@
 
 #include "buildinfo.hpp"
 
-const char * buildinfo::timestamp = "2014-04-30 14:46:15 CEST";
-const char * buildinfo::version = "1.1.1";
+const char * buildinfo::timestamp = "2014-08-13 16:23:05 CEST";
+const char * buildinfo::version = "1.1.2";
