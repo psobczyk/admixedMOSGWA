026ef65b11c4774d6ed679b713633f323f19a06e
Bernhard Bodenstorfer <mosgwa@cogiti.com>
2015-08-22 10:32:44 +0200

    Factored TestLogger out of LoggerTest. Renamed "limit" to "threshold".

--- src/logging/LoggerTest.cpp
+++ src/logging/LoggerTest.cpp
@@ -1,6 +1,6 @@
 /********************************************************************************
  *	This file is part of the MOSGWA program code.				*
- *	Copyright ©2013, Bernhard Bodenstorfer.					*
+ *	Copyright ©2015, Bernhard Bodenstorfer.					*
  *										*
  *	This program is free software; you can redistribute it and/or modify	*
  *	it under the terms of the GNU General Public License as published by	*
@@ -13,62 +13,13 @@
  *	See the GNU General Public License for more details.			*
  ********************************************************************************/
 
-#include "Logger.hpp"
-#include "../TestSuite.hpp"
-#include <cstring>
-#include <cctype>
-#include <string>
+#include "TestLogger.hpp"
 
 using namespace std;
 using namespace unitpp;
 using namespace logging;
 
 namespace test {
-
-	class TestLogger : public Logger, private suite {
-		const char * expected;
-		public:
-		TestLogger ();
-		void setExpect ( const char * const expected );
-		protected:
-		virtual void write ( const char * const text );
-	};
-
-	TestLogger::TestLogger () : suite( "TestLogger" ), expected( NULL ) {
-	}
-
-	void TestLogger::setExpect ( const char * const expected ) {
-		this->expected = expected;
-	}
-
-	void TestLogger::write ( const char * const text ) {
-		if ( NULL == expected ) {
-			assert_fail( "Expected not to forward log" );
-		} else {
-			const char * cursor = text;
-			while( isdigit( *cursor ) ) {
-				++cursor;
-			}
-			assert_true( ">= 4 digits year expected", cursor - text >= 4 );
-			assert_eq( "year-month separator", '-', *cursor++ );
-			assert_true( "month 10", isdigit( *cursor++ ) );
-			assert_true( "month 1", isdigit( *cursor++ ) );
-			assert_eq( "month-day separator", '-', *cursor++ );
-			assert_true( "day 10", isdigit( *cursor++ ) );
-			assert_true( "day 1", isdigit( *cursor++ ) );
-			assert_eq( "day time separator", ' ', *cursor++ );
-			assert_true( "hour 10", isdigit( *cursor++ ) );
-			assert_true( "hour 1", isdigit( *cursor++ ) );
-			assert_eq( "hour:minute separator", ':', *cursor++ );
-			assert_true( "minute 10", isdigit( *cursor++ ) );
-			assert_true( "minute 1", isdigit( *cursor++ ) );
-			assert_eq( "minute:second separator", ':', *cursor++ );
-			assert_true( "second 10", isdigit( *cursor++ ) );
-			assert_true( "second 1", isdigit( *cursor++ ) );
-			assert_eq( "timestamp text separator", '\t', *cursor++ );
-			assert_eq( "log text", string( expected ), string( cursor ) );
-		}
-	}
 
 	/** Tests the class {@link logging::Logger}. */
 	class LoggerTest : public TestSuite {
@@ -95,46 +46,46 @@ namespace test {
 			*debug = "DEBUG\tWobog";
 		TestLogger logger;
 
-		assert_eq( "Default limit INFO", Logger::INFO, logger.getLimit() );
+		assert_eq( "Default threshold INFO", Logger::INFO, logger.getThreshold() );
 
-		logger.setLimit( Logger::ERROR );
-		assert_eq( "New limit ERROR", Logger::ERROR, logger.getLimit() );
-		logger.setExpect( NULL );
+		logger.setThreshold( Logger::ERROR );
+		assert_eq( "New threshold ERROR", Logger::ERROR, logger.getThreshold() );
+		logger.setExpected( NULL );
 		logger.debug( "0" );
 		logger.info( "1" );
 		logger.warning( "2" );
-		logger.setExpect( error );
+		logger.setExpected( error );
 		logger.error( "Whorror" );
 
-		logger.setLimit( Logger::WARNING );
-		assert_eq( "New limit WARNING", Logger::WARNING, logger.getLimit() );
-		logger.setExpect( NULL );
+		logger.setThreshold( Logger::WARNING );
+		assert_eq( "New threshold WARNING", Logger::WARNING, logger.getThreshold() );
+		logger.setExpected( NULL );
 		logger.debug( "3" );
 		logger.info( "4" );
-		logger.setExpect( warning );
+		logger.setExpected( warning );
 		logger.warning( "Whoaning" );
-		logger.setExpect( error );
+		logger.setExpected( error );
 		logger.error( "Whorror" );
 
-		logger.setLimit( Logger::INFO );
-		assert_eq( "New limit INFO", Logger::INFO, logger.getLimit() );
-		logger.setExpect( info );
+		logger.setThreshold( Logger::INFO );
+		assert_eq( "New threshold INFO", Logger::INFO, logger.getThreshold() );
+		logger.setExpected( info );
 		logger.debug( "5" );
 		logger.info( "Whoafo" );
-		logger.setExpect( warning );
+		logger.setExpected( warning );
 		logger.warning( "Whoaning" );
-		logger.setExpect( error );
+		logger.setExpected( error );
 		logger.error( "Whorror" );
 
-		logger.setLimit( Logger::DEBUG );
-		assert_eq( "New limit DEBUG", Logger::DEBUG, logger.getLimit() );
-		logger.setExpect( debug );
+		logger.setThreshold( Logger::DEBUG );
+		assert_eq( "New threshold DEBUG", Logger::DEBUG, logger.getThreshold() );
+		logger.setExpected( debug );
 		logger.debug( "Wobog" );
-		logger.setExpect( info );
+		logger.setExpected( info );
 		logger.info( "Whoafo" );
-		logger.setExpect( warning );
+		logger.setExpected( warning );
 		logger.warning( "Whoaning" );
-		logger.setExpect( error );
+		logger.setExpected( error );
 		logger.error( "Whorror" );
 	}
 
